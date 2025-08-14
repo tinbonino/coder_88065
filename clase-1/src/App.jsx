4cbd34dcdd95d13bc099439ec502a11d7f93292f
martin <tinbonino@gmail.com>
2025-08-13 22:27:52 -0300

    clase-1

---
+++ clase-1/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Bienvenidos a la clase 1 de React</h1>
+        <h2>Gracias por venir</h2>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
